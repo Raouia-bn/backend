dd9f43dd6ca9448bc07576c80c53a4e042feb636
Raouia-bn <raouia.ben19@gmail.com>
2024-12-22 23:39:57 +0100

    mongoooooo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://raouia:raouia@mongo:27017/devops', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://raouia:raouia@mongo:27017/devops')
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
