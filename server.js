256eca906cb7ee40a7b3fd9e828555caabe25817
Raouia-bn <raouia.ben19@gmail.com>
2024-12-23 14:10:11 +0100

    mongo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://raouia:raouia@mongo:27017')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
